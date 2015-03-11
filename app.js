04d2aba541144459fe9587ab4f507eee13282a51
Jonathon Choo <jonno.choo@gmail.com>
2015-03-11 22:53:48 +1100

    Made the port configurable via config.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var router = require('./routes');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', config.express.port);
 app.set("views", __dirname);
 app.set("view engine", "jade");
 
@@ -21,4 +21,6 @@ app.get('/templates/:name', function (req, res) {
 });
 app.use('/', router);
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), function(){
+  console.log("Server stared, listening on", app.get('port'));
+});
--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = module.exports;
 var PRODUCTION = process.env.NODE_ENV === "production";
 
 config.express = {
-    port: process.env.EXPRESS_PORT || 3000
+    port: process.env.EXPRESS_PORT || 3001
 };
 
 if (PRODUCTION) {
